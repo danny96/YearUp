9997aa4d3819add235cfb614735c3af6eeb7c995
danny96 <danielellis246@gmail.com>
2015-04-24 17:01:45 -0400

    First Java Project

---
+++ Intro/src/Intro.java
@@ -0,0 +1,13 @@
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello, I'm Daniel Ellis. Welcome to the Java class.");
+	}
+
+}
+/*The thread was incomplete without the bracket
+ *  and this hindered the program from running properly.
+ *  I didn't close a comment properly and this hindered the program from running.
+ */
